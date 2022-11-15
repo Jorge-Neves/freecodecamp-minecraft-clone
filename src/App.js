d108d0b4a46cd5392c19c188e8a14c0f9e76afea
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-11-15 23:01:32 +0000

    Add Physics container and ambient light to scene

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { Physics } from '@react-three/cannon';
 import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
     <>
-      <div>Outside of canvas</div>
+      <div>Prototype</div>
       <Canvas>
-        <Sky />
+        <Sky sunPosition={[3, 2, 2]} />
+        <ambientLight intensity={0.25} />
+        <Physics></Physics>
       </Canvas>
     </>
   );
---
+++ src/components/Ground.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ground = () => {
+  return (
+    <>
+      <p>ground</p>
+    </>
+  );
+};
+
+export default Ground;
